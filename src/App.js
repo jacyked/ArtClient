f9dfbfff279fb7bb7495ebb515a2b902c2db0779
Matt Jacyk <matthew.jacyk@edu.sait.ca>
2022-11-28 14:44:07 -0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
+
+//Pull photos from drive
+function loadPictures(){
+  //let toAdd = "<div><img src='";
+  //let success = true;
+  axios
+  .get('http://localhost:5000/photos')
+  .then(function (res) {
+    //let toAdd = "<div><img src='";
+    console.log(res);
+    console.log(res.data);
+    res.data.forEach(displayAndParse);
+
+
+    function displayAndParse(array){
+      array.forEach(function (item){
+        console.log(item);
+        const arr = item.split(":");
+        let id = arr[1];
+        let name = arr[0];
+        let x = 'https://drive.google.com/uc?export=view&id=' + id
+        //JSON.parse(item);
+        //console.log(item.webViewLink);
+        //toAdd = toAdd + item.webViewLink + "' alt='" + item.name +"'></div><div><img src='";
+        const fig = document.createElement("figure");
+        const cap = document.createElement("figcaption");
+        const n = document.createTextNode(name);
+        cap.appendChild(n);
+        const image = new Image(400,400);
+        image.src = x;
+        fig.appendChild(image);
+        fig.appendChild(cap);
+        //image.crossOrigin = "Access-Control-Allow-Origin";
+        document.getElementById('images').appendChild(fig);
+        //image.crossOrigin
+      });
+    };
+    //sendToPage(toAdd);
+  })
+  .catch(err => {
+    console.error(err);
+    //success = false;
+  });
+  //ReactDOM.render(toAdd, document.getElementById('images'));
+ 
+  
+
+};
+
+function sendToPage(toAdd){
+  let e = document.getElementById('images');
+  e.innerHTML = toAdd;
+}
 
 function App() {
   return (
@@ -17,7 +72,10 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={loadPictures}>Load Pictures</button>
+        <div id='images'></div>
       </header>
+      
     </div>
   );
 }
